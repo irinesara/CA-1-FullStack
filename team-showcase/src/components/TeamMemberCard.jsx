19e462de6dae36b01e784dee5d4daa8af987c54c
irinesara <irine.ixa@gmail.com>
2025-02-27 10:46:57 +0530

    ok

---
+++ team-showcase/src/components/TeamMemberCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TeamMemberCard = () => {
+  return (
+    <div className='details'>
+        <h1> Team Member Name: </h1>
+        <h2>Job Title: </h2>
+
+    </div>
+  )
+}
+
+
+export default TeamMemberCard
